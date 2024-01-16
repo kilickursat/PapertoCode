271d3c37f85647d8f1643b1705218a6bf715c59c
Kursat Kilic <kilic_kursat@hotmail.com>
2024-01-16 14:22:27 +0900

    Update PapertoCode.py

--- PapertoCode.py
+++ PapertoCode.py
@@ -8,7 +8,7 @@ from PIL import Image
 st.title('My Streamlit App')
 
 # Load the image using PIL
-image_path = '/mnt/data/An_artificial_neural_network_with_covered_by_science_and_scientific_articles_the_ANN_swallow_the_knowledge_from_the_papers_like_a_black_hole__style-_Kon_style-Isometric_seed-0ts-1705245629_idx-0.png'
+image_path = 'An_artificial_neural_network_with_covered_by_science_and_scientific_articles_the_ANN_swallow_the_knowledge_from_the_papers_like_a_black_hole__style-_Kon_style-Isometric_seed-0ts-1705245629_idx-0.png'
 
 image = Image.open(image_path)
 
